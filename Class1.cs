87ad1497cec370c69dea20026c3a191347ba600c
Snehal Sarode <snehalsarode1804@gmail.com>
2025-07-02 18:23:05 +0530

    committed class1

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalyani_project_for_git
+{
+    internal class Class1
+    {
+    }
+}
